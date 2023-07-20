3ac7110bf476abcc5ac5b11237d55fb148de8a9a
Sangho Lee <Sangho.Lee@microsoft.com>
2023-07-20 10:21:58 -0700

    update build preparation script

--- .pipelines/prepare.sh
+++ .pipelines/prepare.sh
@@ -4,7 +4,6 @@
 
 sudo apt update
 sudo apt install -y \
-	wget \
 	build-essential \
 	libncurses-dev \
 	bison \
@@ -15,4 +14,48 @@ sudo apt install -y \
 	gcc-arm-linux-gnueabihf \
 	binutils-aarch64-linux-gnu
 
-pip3 install pycryptodomex
+sudo apt install -y \
+	adb \
+	acpica-tools \
+	autoconf \
+	automake \
+	bc \
+	ccache \
+	cscope \
+	curl \
+	device-tree-compiler \
+	e2tools \
+	expect \
+	fastboot \
+	ftp-upload \
+	gdisk \
+	libattr1-dev \
+	libcap-dev \
+	libfdt-dev \
+	libftdi-dev \
+	libglib2.0-dev \
+	libgmp3-dev \
+	libhidapi-dev \
+	libmpc-dev \
+	libncurses5-dev \
+	libpixman-1-dev \
+	libslirp-dev \
+	libtool \
+	libusb-1.0-0-dev \
+	make \
+	mtools \
+	netcat \
+	ninja-build \
+	python3-cryptography \
+	python3-pip \
+	python3-pyelftools \
+	python3-serial \
+	python-is-python3 \
+	rsync \
+	swig \
+	unzip \
+	uuid-dev \
+	xdg-utils \
+	xterm \
+	xz-utils \
+	zlib1g-dev
